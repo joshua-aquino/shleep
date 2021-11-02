852444df7339fd57be186d2ec6f88466d12e916e
Joshua Aquino <joshua-aquino@pm.me>
2021-11-02 11:13:13 -0400

    start color animations

--- app/src/main/java/com/example/shleep/MainActivity.java
+++ app/src/main/java/com/example/shleep/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.shleep;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,29 +14,54 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String darkColor = "#000000";
+    private String lightColor = "#FFCAB0";
+    private boolean isShleeping;
+    private ConstraintLayout shleepApp;
     private Button shleepBtn;
     private TextView shleepTxt;
-    private boolean isShleeping;
+    private ValueAnimator colorAnimation;
+
+    private void shleepNow() {
+        if (isShleeping == false) {
+            isShleeping = true;
+            shleepTxt.setText("What's up?");
+            colorAnimation.start();
+        } else {
+            isShleeping = false;
+            shleepTxt.setText("Shleep.");
+            shleepApp.setBackgroundColor(Color.parseColor(darkColor));
+        }
+    }
+
+    private void changeColors() {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        shleepApp = (ConstraintLayout) findViewById(R.id.shleepApp);
         shleepBtn = (Button) findViewById(R.id.shleepBtn);
         shleepTxt = (TextView) findViewById(R.id.shleepTxt);
         isShleeping = false;
+        colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(),
+                Color.parseColor(darkColor), Color.parseColor(lightColor));
+        colorAnimation.setDuration(10000);
+        colorAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                shleepApp.setBackgroundColor((int) animation.getAnimatedValue());
+            }
+        });
 
         shleepBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (isShleeping == false) {
-                    isShleeping = true;
-                    shleepTxt.setText("What's up?");
-                } else {
-                    isShleeping = false;
-                    shleepTxt.setText("shleep.");
-                }
+                shleepNow();
             }
         });
     }
