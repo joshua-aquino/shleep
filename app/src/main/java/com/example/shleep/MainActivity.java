2a9e250e39b8bbcd9deb49196362b54a3233b7d9
Joshua Aquino <joshua-aquino@pm.me>
2021-11-02 10:19:20 -0400

    start button functionalities

--- app/src/main/java/com/example/shleep/MainActivity.java
+++ app/src/main/java/com/example/shleep/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.shleep;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button shleepBtn;
+    private TextView shleepTxt;
+    private boolean isShleeping;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        shleepBtn = (Button) findViewById(R.id.shleepBtn);
+        shleepTxt = (TextView) findViewById(R.id.shleepTxt);
+        isShleeping = false;
+
+        shleepBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isShleeping == false) {
+                    isShleeping = true;
+                    shleepTxt.setText("What's up?");
+                } else {
+                    isShleeping = false;
+                    shleepTxt.setText("shleep.");
+                }
+            }
+        });
     }
 }
