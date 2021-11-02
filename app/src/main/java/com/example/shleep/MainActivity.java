bddf8d20309dfe5b7e6667c20c5877ee537e1ccb
Joshua Aquino <joshua-aquino@pm.me>
2021-11-02 15:54:04 -0400

    add auto-off functionality

--- app/src/main/java/com/example/shleep/MainActivity.java
+++ app/src/main/java/com/example/shleep/MainActivity.java
@@ -2,45 +2,55 @@ package com.example.shleep;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
-
 import android.animation.ArgbEvaluator;
-import android.animation.TimeInterpolator;
 import android.animation.ValueAnimator;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.view.animation.AccelerateInterpolator;
 import android.view.animation.Animation;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private String darkColor = "#000000";
     private String lightColor = "#FFCAB0";
-    private boolean isShleeping;
+    private boolean isShleeping = false;
     private ConstraintLayout shleepApp;
     private Button shleepBtn;
     private TextView shleepTxt;
     private ValueAnimator colorAnimation;
+    private CountDownTimer shleepTimer;
 
     private void shleepNow() {
         if (isShleeping == false) {
             isShleeping = true;
             shleepTxt.setText("What's up?");
+
             setAnimation(darkColor, lightColor, 8000);
             colorAnimation.setRepeatMode(ValueAnimator.REVERSE);
             colorAnimation.setInterpolator(new AccelerateInterpolator());
             colorAnimation.setRepeatCount(Animation.INFINITE);
-            colorAnimation.start();
+            shleepTimer = new CountDownTimer(10000, 1000) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+                }
+                @Override
+                public void onFinish() {
+                    shleepNow();
+                }
+            }.start();
         } else {
             isShleeping = false;
             shleepTxt.setText("Shleep.");
+
             colorAnimation.cancel();
             setAnimation(lightColor, darkColor, 1000);
-            colorAnimation.start();
+            shleepTimer.cancel();
         }
+        colorAnimation.start();
     }
 
     private void setAnimation(String startColor, String endColor, long speed) {
@@ -64,7 +74,6 @@ public class MainActivity extends AppCompatActivity {
         shleepApp = (ConstraintLayout) findViewById(R.id.shleepApp);
         shleepBtn = (Button) findViewById(R.id.shleepBtn);
         shleepTxt = (TextView) findViewById(R.id.shleepTxt);
-        isShleeping = false;
 
         shleepBtn.setOnClickListener(new View.OnClickListener() {
             @Override
